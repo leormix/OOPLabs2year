56e014c139abb7f8678999923258582f2c8cb423
leormix <82319032+leormix@users.noreply.github.com>
2026-09-19 12:22:33 +0300

    Fuck C++ all my homies write on python

--- 1st/module1/module1.h
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-#ifndef MODULE1_H
-#define MODULE1_H
-
-extern int Func_MOD2_1(HWND hWnd);
-
-#endif
--- 1st/module2/module2.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULE2_H
-#define MODULE2_H
-
-#include <windows.h>
-
-extern int Func_MOD2_2(HWND hWnd);
-
-#endif
